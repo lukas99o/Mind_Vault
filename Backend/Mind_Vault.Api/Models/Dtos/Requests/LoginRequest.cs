5f120610062f6a27392db83b49e8efccc2a556ad
Lukas <lukas99o@hotmail.com>
2026-05-14 08:39:43 +0200

    feat: Enhance LoginRequest and RegisterRequest with data annotations for validation

--- Backend/Mind_Vault.Api/Models/Dtos/Requests/LoginRequest.cs
+++ Backend/Mind_Vault.Api/Models/Dtos/Requests/LoginRequest.cs
@@ -1,3 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace Mind_Vault.Api.Models.Dtos;
 
-public sealed record LoginRequest(string Email, string Password);
+public sealed record LoginRequest(
+    [property: EmailAddress]
+    [property: Required]
+    string Email, 
+    
+    [property: Required]
+    [property: MinLength(8)]
+    string Password
+);
--- Backend/Mind_Vault.Api/Models/Dtos/Requests/RegisterRequest.cs
+++ Backend/Mind_Vault.Api/Models/Dtos/Requests/RegisterRequest.cs
@@ -1,3 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mind_Vault.Api.Models.Dtos;
 
-public sealed record RegisterRequest(string Email, string Password);
+public sealed record RegisterRequest(
+    [property: Required]
+    [property: EmailAddress] 
+    string Email, 
+    
+    [property: Required]
+    [property: MinLength(8)]
+    string Password
+);
